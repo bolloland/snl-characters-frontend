bd2feebd891fc595d7b377640ab78e3e0ff38c3e
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-09-10 22:12:49 -0500

    fetchMembers  written member.js

--- src/member.js
+++ src/member.js
@@ -1,3 +1,4 @@
+const memberURL = "http://localhost:3000/members"
 
 class Member {
 
@@ -12,6 +13,33 @@ class Member {
         this.characters = characters
 
         Member.all.push(this)
+    }
+
+    renderMember = () => {
+        console.log(this) // <an instance
+        let actorsContainer = document.getElementById("actor-container")
+        let div = document.getElementById("div")
+        actorsContainer.innerHTML += `<div><img class="memberImage" src=${this.image} /><div>test</div></div>`
 
     }
-}
+
+    static fetchMembers = () => {
+    fetch(memberURL)
+    .then(resp => resp.json())
+    .then(json => {
+        json.forEach(player => {
+        let mem = new Member(player.id, player.first, player.last, player.joined, player.left, player.image, player.characters)
+        console.log(mem)
+        mem.renderMember()
+        //because we're in Class, variable has the method called on it. 
+        //If we were in index.js, renderMember(mem) would be appropo
+        })
+      })
+    }
+    }
+
+
+
+
+
+
