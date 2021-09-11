665cc23a2430c9a074ddfd535ef7ba35a8ca482a
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-09-10 23:50:07 -0500

    postMembewr, newMember

--- src/member.js
+++ src/member.js
@@ -1,7 +1,7 @@
 const memberURL = "http://localhost:3000/members"
 
 class Member {
-
+    
     static all = []
     constructor(id, first, last, joined, left, image, characters) {
         this.id = id
@@ -15,11 +15,55 @@ class Member {
         Member.all.push(this)
     }
 
-    renderMember = () => {
+    static postMember = (member) => {
+        fetch(memberURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+              "Accept": "application/json",
+            },
+            body: JSON.stringify(member)
+          })
+          
+          .then(resp => resp.json())
+          .then(data => {
+            let mem = new Member(data.id, data.first, data.last, data.joined, data.left, data.image, data.characters)
+            mem.renderMember()
+             memberFirstValue = ""
+             memberLastValue = ""
+             memberJoinedValue = ""
+             memberLeftValue = ""
+             memberImageValue = ""
+             
+          })
+          .catch(error => {
+              alert("Warning! Danger, Will Robinson!");
+              document.body.innerHTML = error.message;
+            })
+        }
+        
+        static newMember = (e) => {
+            e.preventDefault()
+            console.log("aroo?")
+            
+            let memberFirstValue = document.getElementById("memberFirst").value
+            let memberLastValue = document.getElementById("memberLast").value
+            let memberJoinedValue = document.getElementById("memberJoined").value
+            let memberLeftValue = document.getElementById("memberLeft").value
+            let memberImageValue = document.getElementById("memberImage").value
+            
+            let member = {first: memberFirstValue, last: memberLastValue, joined: memberJoinedValue, left: memberLeftValue, image: memberImageValue}
+            
+            Member.postMember(member)
+            
+        }
+        
+        
+        renderMember = () => {
         console.log(this) // <an instance
         let actorsContainer = document.getElementById("actor-container")
         let div = document.getElementById("div")
-        actorsContainer.innerHTML += `<div><img class="memberImage" src=${this.image} /><div>test</div></div>`
+        actorsContainer.innerHTML += `<div><img class="memberImage" src=${this.image} /><div>${this.first} ${this.last}</div></div>`
 
     }
 
