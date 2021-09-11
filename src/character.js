68f49c2b35df7009448d2cbd9bc06384353002c7
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-09-10 21:13:11 -0500

    create Character class

---
+++ src/character.js
@@ -0,0 +1,20 @@
+
+class Character {
+
+    static all = []
+    constructor(id, name, debut, review, image, script_one, script_two, script_three, likes, member_id) {
+        this.id = id
+        this.name = name
+        this.debut = debut
+        this.review = review
+        this.image = image
+        this.script_one = script_one
+        this.script_two = script_two
+        this.script_three = script_three
+        this.likes = likes
+        this.member_id = member_id
+
+        Character.all.push(this)
+
+    }
+}
