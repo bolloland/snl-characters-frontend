eee159a022b6e5120b2bc4054460d482c3bac806
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-09-17 11:40:24 -0500

    fetchCharacters( base created

--- src/character.js
+++ src/character.js
@@ -1,6 +1,7 @@
 
-class Character {
 
+class Character {
+    
     static all = []
     constructor(id, name, debut, review, image, script_one, script_two, script_three, likes, member_id) {
         this.id = id
@@ -13,21 +14,42 @@ class Character {
         this.script_three = script_three
         this.likes = likes
         this.member_id = member_id
-
+        
         Character.all.push(this)
-
     }
+    
+    
+ 
+    // renderMember = () => {
+    //     // console.log(this) // <an instance
+    //     let actorsContainer = document.getElementById("actor-container")
+    //     let div = document.getElementById("div")
+    //     actorsContainer.innerHTML += `<div class="memberContainer">
+    //         <img class="memberImage" src=${this.image} /><div>
+    //         <div>${this.first} ${this.last}</div>
+    //         <button id=${this.id} class="show-character" onclick="Character.showCharacters()" type="button">characters</button>
+    //         <button id=${this.id} class="edit-member" onclick="Member.editMember()" type="button">edit</button>
+    //         <button id=${this.id} onclick="Member.deleteMember()" type="button" style="color: red">&#10060;</button>
+    //         </div></div>`
 
-    static showCharacters = () => {
-        console.log("showing characters")
-        let charBlock = document.getElementById("character-container")
-        let actorsContainer = document.getElementById("actor-container")
-        charBlock.hidden = false
-        actorsContainer.hidden = true
-    }
+    // }
 
-
-
+    static fetchCharacters = (e) => {
+        console.log(e)
+        fetch(memberURL)
+        .then(resp => resp.json())
+        .then(json => console.log(json))
+     
+            // {
+        //     json.forEach(player => {
+        //     let mem = new Member(player.id, player.first, player.last, player.joined, player.left, player.image, player.characters)
+        //     console.log(mem)
+        //     mem.renderMember()
+        //     //because we're in Class, variable has the method called on it. 
+        //     //If we were in index.js, renderMember(mem) would be appropo
+        //     })
+        //   })
+        }
 
 
 
