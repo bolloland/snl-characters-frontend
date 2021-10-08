81a046de7efe79c8211536c99fe469e0371af4ab
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-10-07 20:33:27 -0500

    add postChar fetch and fix newChar form

--- src/character.js
+++ src/character.js
@@ -19,54 +19,55 @@ class Character {
     }
     
     static postCharacter = (newChar) => {
-      preventDefault()
+     
+            //fetch method to post new character info
       console.log(newChar)
+      return fetch(charURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+              "Accept": "application/json",
+            },
+            body: JSON.stringify(newChar)
+          })
+          .then(resp => resp.json())
+          .then(data => {
+            let newChara = new Character(data.id, data.name, data.debut, data.review, data.image, data.script_one, data.script_two, data.script_three, data.likes, data.member_id)
+            newChara.renderCharacters()
+            
+          })
+          
+          // let clearMemberForm = document.getElementById("addMember")
+          // clearMemberForm.reset()
     }
 
 // ADD NEW CHARACTERS //
-// let newCharForm = document.getElementById("#add-Char")
-
-      
-    static showForm = (e) => {
-      console.log(e)    
-      let owner = Member.all.find((owner) => owner.id == e) 
-      console.log(owner)
-      console.log(`owner.id is ${owner.id}` )
-      document.querySelector("#actor-container").classList.toggle("hidden-member") //hide actor container
-      let newCharFormCont = document.querySelector("#new-char-form-cont").classList.toggle("hidden-char-form")
-    }
-
-    
-
     static newCharacter = (e) => {
       console.log(e)
   
       let owner = Member.all.find((owner) => owner.id == e) 
-      console.log(owner)
+      let ownerId = owner.id
+      // console.log(owner)
       console.log(`owner.id is ${owner.id}` )
       document.querySelector("#actor-container").classList.toggle("hidden-member") //hide actor container
       let newCharFormCont = document.querySelector("#new-char-form-cont").classList.toggle("hidden-char-form")
       
-      // let newCharSubmitButton = document.querySelector("#newCharSubmitButton")
-      newCharSubmitButton.addEventListener("click", console.log("good lord willing"))
-      // let newCharForm = document.getElementById("#add-Char")
-      // newCharForm.addEventListener("submit", console.log("hey man"))
+      let newCharSubmitButton = document.getElementById("add-Char")
+      newCharSubmitButton.addEventListener("submit", (e) => {
+        e.preventDefault()
+        // let newCharName = e.target.name.value
+        // let newCharImage = e.target.image.value
+        // let charLikes = 0
+        let newChar = {name: e.target.name.value, image: e.target.image.value, member_id: ownerId, likes: 0} 
+        this.postCharacter(newChar)
+        e.target.reset() 
 
-
-      // let newCharNameValue = document.getElementById("newCharName").value
-      // let newCharImageValue = document.getElementById("newCharImage").value
-
-      // let newChar = {name: newCharNameValue, image: newCharNameImage, member_id: owner.id}
-      // debugger
-       
-        // document.getElementById("new-char-form-cont").hidden = true
-      //   newCharForm.addEventListener("submit", () => {
-      //     console.log("submitting character info to Post")
-      // let newCharNameValue = document.getElementById("newCharName").value
-      // let newCharImageValue = document.getElementById("newCharImage").value
-      // let newChar = {name: newCharNameValue, image: newCharNameImage, member_id: owner.id}
-      // Character.postCharacter(newChar)
-      //   })
+        console.log(ownerId)
+        // this.postCharacters
+        document.querySelector("#actor-container").classList.toggle("hidden-member")
+        this.fetchCharacters(ownerId)
+      })
+    
     }
 
 // LIKE BUTTON //
@@ -118,11 +119,13 @@ class Character {
     }
     
     static fetchCharacters = (e) => {
+     
         document.querySelector("#actor-container").classList.toggle("hidden-member")
         document.querySelector("#character-container").classList.toggle("hidden")
         document.querySelector("#newMemberFormButton").classList.toggle("new-member-button")
         let creator = Member.all.find(mem => mem.id == e)
         // getMemberName(creator)
+        //
         let charID = parseInt(e)
         // console.log(charID)
         fetch(`${memberURL}/${charID}`)
